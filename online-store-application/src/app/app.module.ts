d1e8ca4e508526b2e36ce1d29ad2753742b65d69
Hujaj Ali <hujaj.ali@appinsnap.com>
2023-09-14 10:37:57 +0500

    project setup and ui resources for online shop ecommerce app

---
+++ online-store-application/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationBarComponent } from './components/shared/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavigationBarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
